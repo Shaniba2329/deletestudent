f77e808a58a26f8db18900bf42799632c1e85953
shanibamariyam <shanibamariyam00@gmail.com>
2024-06-09 12:12:55 +0530

    completed

---
+++ ViewAllbook.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const ViewAllbook = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Title</th>
+      <th scope="col">Author</th>
+      <th scope="col">Published_year</th>
+      <th scope="col">price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>1976</td>
+      <td>500</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>1987</td>
+      <td>700</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>1886</td>
+      <td>560</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>mary loise</td>
+      <td>1888</td>
+      <td>850</td>
+    </tr> 
+    <tr>
+      <th scope="row">5</th>
+      <td>Jacon</td>
+      <td>1999</td>
+      <td>600</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>James john</td>
+      <td>1995</td>
+      <td>370</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>manes</td>
+      <td>1998</td>
+      <td>450</td>
+    </tr> 
+     <tr>
+      <th scope="row">8</th>
+      <td>naimon</td>
+      <td>1967</td>
+      <td>670</td>
+    </tr>
+
+  </tbody>
+</table>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewAllbook
